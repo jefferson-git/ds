b8b46db8832b2a33235f05e274a14e0f7526265c
jefferson-git <jefferson-goiania@hotmail.com>
2022-07-15 13:06:39 -0300

    date update

--- frontend/src/components/SelesCard/SelesCard.tsx
+++ frontend/src/components/SelesCard/SelesCard.tsx
@@ -9,15 +9,22 @@ import { BASE_URL } from "../../utils/request";
 import { Sale } from "../../models/sale";
 
 function SelesCard() {
-  const [minDate, setMinDate] = useState(new Date());
+  const [minDate, setMinDate] = useState(
+    new Date(new Date().setDate(new Date().getDate() - 365))
+  );
   const [maxDate, setMaxDate] = useState(new Date());
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sale`).then((response) => {
-      setSales(response.data.content);
-    });
-  }, []);
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios
+      .get(`${BASE_URL}/sale?minDate=${dmin}&maxDate=${dmax}`)
+      .then((response) => {
+        setSales(response.data.content);
+      });
+  }, [minDate, maxDate]);
 
   return (
     <>
