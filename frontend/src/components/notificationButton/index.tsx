a6e7c59e003fdbb3bbd036c1cb14ac178c2a0a9f
jefferson-git <jefferson-goiania@hotmail.com>
2022-07-15 14:26:39 -0300

    Toast

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,11 +1,23 @@
+import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
 
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function handelClick(id: number) {
+  axios(`${BASE_URL}/sale/${id}/notification`).then((response) => {
+    console.log("foi blz");
+  });
+}
+
+function NotificationButton({ saleId }: Props) {
   return (
     <>
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={() => handelClick(saleId)}>
         <img src={icon} alt="Notificar" />
       </div>
     </>
